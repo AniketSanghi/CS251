f31ea72e1ae192b84f7ea59b10d104d23a390965
Aniket Sanghi <sanghi@iitk.ac.in>
2019-04-27 22:52:33 +0530

    Update ass8.hs

--- CS251_Ass_8/ass8.hs
+++ CS251_Ass_8/ass8.hs
@@ -8,6 +8,6 @@ incrrange x (a,b) = take (a-1) x ++ (map (+1).drop (a-1).take b$x) ++ drop b x
 combinations n [] = [[]]
 combinations 0 x = [[]]
 combinations n (x:xs) 
-    | xs < n = map (x:).combinations (n-1)$xs
+    | len < n = map (x:).combinations (n-1)$xs
     | otherwise = (map (x:) (combinations (n-1) xs)) ++ (combinations n xs)
     where len = length xs 
